66467a93749bab41e4b22a84776dcb3a36d141f5
Gl00ria <gl00ria.bbh@gmail.com>
2024-05-08 16:07:22 +0300

    add: inlayhint autocmd 'nvim-nighlty'

--- lua/Gl0riVim/config/autocmds.lua
+++ lua/Gl0riVim/config/autocmds.lua
@@ -94,3 +94,14 @@ autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "*.txt", "*.md", "*.json" },
   command = "setlocal conceallevel=0",
 })
+
+-- inlayhints
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true)
+    end
+  end,
+})
